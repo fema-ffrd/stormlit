37e5212e7a7a91a5e1a953b2fea017edb35557db
Seth Lawler <slawler@dewberry.com>
2025-01-23 12:58:22 +0000

    add stac_browser and stac_api to session state

--- app/src/utils/session.py
+++ app/src/utils/session.py
@@ -1,5 +1,7 @@
-import streamlit as st
+import os
 from datetime import datetime
+
+import streamlit as st
 
 
 def init_session_state():
@@ -13,3 +15,5 @@ def init_session_state():
     st.session_state["realization"] = None
     st.session_state["block"] = None
     st.session_state["search_id"] = None
+    st.session_state["stac_api"] = os.getenv("STAC_API_URL")
+    st.session_state["stac_browser"] = os.getenv("STAC_BROWSER_URL")
