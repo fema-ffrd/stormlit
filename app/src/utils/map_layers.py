c14cac240335af749f30c115e55f0df4a16e5754
zherbz <zherbz32@gmail.com>
2025-03-26 17:35:23 +0000

    temporary assets until data is migrated into stac catalog

---
+++ app/src/utils/map_layers.py
@@ -0,0 +1,5 @@
+trinity = {
+    "Basins": "/workspace/app/src/assets/TrinityHUC8.geojson",
+    "Dams": "/workspace/app/src/assets/TrinityDams.geojson",
+    "Gages": "https://trinity-pilot.s3.amazonaws.com/stac/prod-support/gages/gages.geojson",
+}
