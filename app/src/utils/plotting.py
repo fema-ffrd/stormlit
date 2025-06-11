7f9597090b3c9fe9c903f113ecb18ddf31ee2702
Zach Herbert <zachary.herbert@mbakerintl.com>
2025-06-11 16:45:39 +0000

    moved all plotting functions here

--- app/src/utils/plotting.py
+++ app/src/utils/plotting.py
@@ -1,25 +1,151 @@
-import plotly.express as px
+import streamlit as st
 import pandas as pd
+from plotly import express as px
+import plotly.graph_objects as go
+from typing import Optional
 
 
-def create_time_series_plot(df: pd.DataFrame):
+def plot_ts(
+    df1: pd.DataFrame,
+    df2: pd.DataFrame,
+    var1: str,
+    var2: str,
+    dual_y_axis: bool,
+    title: Optional[str] = None,
+):
+    """Function for plotting time series data from two DataFrames with dual y-axes.
+    Each DataFrame should have:
+    - 'time': datetime
+    - var1: float (for df1)
+    - var2: float (for df2)
+    Parameters
+    ----------
+    df1 : pd.DataFrame
+        The first DataFrame containing the time series data.
+    df2 : pd.DataFrame
+        The second DataFrame containing the time series data.
+    var1 : str
+        The first variable to plot.
+    var2 : str
+        The second variable to plot.
+    dual_y_axis : bool
+        Whether to plot the second variable on a secondary y-axis.
+    title : Optional[str]
+        The title of the plot. If None, a default title will be used.
     """
-    Create a Plotly time series plot from a DataFrame.
+    # Check if the DataFrames are empty
+    if df1.empty and df2.empty:
+        st.warning("No data available for the selected variables in either dataset.")
+        return
+
+    fig = go.Figure()
+
+    if not dual_y_axis:
+        # Add traces for the first DataFrame
+        if not df1.empty:
+            fig.add_trace(
+                go.Scatter(
+                    x=df1["time"],
+                    y=df1[var1],
+                    mode="lines",
+                    name=f"{var1}",
+                    line=dict(color="red"),
+                    yaxis="y1",
+                )
+            )
+
+        # Add traces for the second DataFrame
+        if not df2.empty:
+            fig.add_trace(
+                go.Scatter(
+                    x=df2["time"],
+                    y=df2[var2],
+                    mode="lines",
+                    name=f"{var2}",
+                    line=dict(color="blue"),
+                    yaxis="y1",
+                )
+            )
+    else:
+        # Add traces for the first DataFrame
+        if not df1.empty:
+            fig.add_trace(
+                go.Scatter(
+                    x=df1["time"],
+                    y=df1[var1],
+                    mode="lines",
+                    name=f"{var1}",
+                    line=dict(color="red"),
+                    yaxis="y1",
+                )
+            )
+
+        # Add traces for the second DataFrame
+        if not df2.empty:
+            fig.add_trace(
+                go.Scatter(
+                    x=df2["time"],
+                    y=df2[var2],
+                    mode="lines",
+                    name=f"{var2}",
+                    line=dict(color="blue"),
+                    yaxis="y2",
+                )
+            )
+        # Update layout for dual y-axes
+        fig.update_layout(
+            title=title if title else f"Time Series Plot of {var1} and {var2}",
+            xaxis_title="Time",
+            yaxis=dict(
+                title=var1,
+                showgrid=False,
+                zeroline=True,
+            ),
+            yaxis2=dict(
+                title=var2,
+                overlaying="y",
+                side="right",
+                showgrid=False,
+                zeroline=True,
+            ),
+            legend=dict(x=0.75, y=1, traceorder="normal"),
+        )
+
+    st.plotly_chart(fig)
+
+
+def plot_hist(df: pd.DataFrame, x_col: str, y_col: str, nbins: int):
+    """
+    Function for plotting histogram data.
 
     Parameters
     ----------
     df : pd.DataFrame
-        DataFrame containing the time series data with datetime index.
-        Single column DataFrame with datetime index.
-
-    Returns
-    -------
-    fig : plotly.graph_objects.Figure
-        Plotly figure object.
+        The DataFrame containing the histogram data.
+    x_col : str
+        The column name for the x-axis values.
+    y_col : str
+        The column name for the y-axis values.
+    nbins : int
+        The number of bins for the histogram.
     """
-    target_col = df.columns[0]
-    df["date"] = df.index
-    df = df.reset_index(drop=True)
-    # Create the Plotly figure
-    fig = px.line(df, x="date", y=target_col, title="Gage Period of Record")
+    # Check if the DataFrame is empty
+    if df.empty:
+        st.warning("No data available for the selected variable.")
+        return
+    fig = px.histogram(
+        df,
+        nbins=nbins,
+        x=x_col,
+        y=y_col,
+        title="Histogram of Raster Values",
+        labels={x_col: "COG Value", y_col: "Count"},
+    )
+    fig.update_traces(marker_color="blue")
+    fig.update_layout(
+        xaxis_title=x_col,
+        yaxis_title=y_col,
+        showlegend=True,
+    )
     return fig
+
