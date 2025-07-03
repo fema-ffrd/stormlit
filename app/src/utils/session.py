8b865a1dd9ba27befb3b114cec2f9f162fd6ce5f
Zach Herbert <zachary.herbert@mbakerintl.com>
2025-07-03 14:47:42 +0000

    added default block range

--- app/src/utils/session.py
+++ app/src/utils/session.py
@@ -43,6 +43,7 @@ def init_session_state():
     st.session_state["gage_event"] = None
     st.session_state["stochastic_event"] = None
     st.session_state["stochastic_storm"] = None
+    st.session_state["block_range"] = (50, 100)
 
     # model qc session
     st.session_state["model_qc_file_path"] = None
