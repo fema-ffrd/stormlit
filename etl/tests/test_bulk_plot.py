210ac3a4dd28209c178c9f7fbb79bd29407ba8e2
Taher Chegini <tchegini@dewberry.com>
2025-08-13 17:52:41 -0400

    chore: fix the number of plots.

--- etl/tests/test_bulk_plot.py
+++ etl/tests/test_bulk_plot.py
@@ -1,9 +1,12 @@
+from __future__ import annotations
+
 import shutil
 from pathlib import Path
 
 from bulk_plot import plot_all_hms_elements
 
+
 def test_plot_all_hms_elements():
     plot_all_hms_elements(save_dir="plots")
-    assert len(list(Path("plots").glob("*.png"))) == 42
+    assert len(list(Path("plots").glob("*.png"))) == 95
     shutil.rmtree("plots")
