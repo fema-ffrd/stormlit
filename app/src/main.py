c2468b3380fca0b19140b4d8e7e0a83fde0bcb5c
Zach Herbert <zachary.herbert@mbakerintl.com>
2025-11-05 21:43:37 +0000

    removed test page reference

--- app/src/main.py
+++ app/src/main.py
@@ -30,9 +30,6 @@ def home_page():
     st.sidebar.page_link("pages/model_qc.py", label="Model QC")
     st.sidebar.page_link("pages/hms_results.py", label="HMS Results")
     st.sidebar.page_link("pages/ras_results.py", label="RAS Results")
-    # st.sidebar.page_link("pages/all_results.py", label="All Results")
-    # TESTING
-    st.sidebar.page_link("pages/test_page.py", label="Test Page")
 
     database_link_dict = {
         "FFRD Cloud": "https://ffrd.cloud.dewberryanalytics.com/",
